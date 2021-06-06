fac29e47ce71acca7374d4a002fded41a829a222
LoTwT <709937065@qq.com>
2021-06-06 17:52:58 +0800

    FormInput

---
+++ src/components/FormInput.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="field">
+    <label :for="name" class="label">{{ name }}</label>
+    <div class="control">
+      <input class="input" :type="type" :id="name" @input="handleInput" :value="modelValue" />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue"
+
+export default defineComponent({
+  name: "FormInput",
+  props: {
+    type: {
+      type: String,
+      default: "text",
+    },
+    name: {
+      type: String,
+      required: true
+    },
+    modelValue: {
+      type: String,
+      required: true
+    }
+  },
+  setup(props, ctx) {
+    const handleInput = (event: any) => {
+      ctx.emit("update:modelValue", event.target.value)
+    }
+
+    return {
+      handleInput
+    }
+  }
+})
+</script>
